5ff7c72800360005ed6a455940e06cea6b3ff308
Ivan <110188349+IvanIksanov@users.noreply.github.com>
2023-12-21 20:56:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,33 @@ $('.row .thumbnail').click(function(){
 		$('#myModal').modal('show'); // show the modal
 		$('#modalCarousel').carousel(id); // slide carousel to selected
 });
+
+// Создаем элемент снежинки
+function createSnowflake() {
+  const snowflake = document.createElement('div');
+  snowflake.innerHTML = '❄'; // Используем символ снежинки
+  snowflake.style.position = 'fixed';
+  snowflake.style.color = 'white';
+  snowflake.style.fontSize = '20px';
+  snowflake.style.zIndex = '9999';
+
+  // Устанавливаем случайное начальное положение снежинки
+  const startX = Math.random() * window.innerWidth;
+  snowflake.style.left = startX + 'px';
+  snowflake.style.top = '-50px';
+
+  // Устанавливаем случайную скорость падения снежинки
+  const speed = Math.random() * 5 + 1;
+  snowflake.style.animation = `snowfall ${speed}s linear infinite`;
+
+  // Добавляем снежинку на страницу
+  document.body.appendChild(snowflake);
+}
+
+// Генерируем снежинки
+function generateSnowflakes() {
+  setInterval(createSnowflake, 500); // Создаем новую снежинку каждые 0.5 секунды
+}
+
+// Запускаем генерацию снежинок после полной загрузки страницы
+window.addEventListener('load', generateSnowflakes);
