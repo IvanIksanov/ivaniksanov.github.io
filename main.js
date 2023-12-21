063370cf22e1bb46bff886a00409a3ed300224fc
Ivan <110188349+IvanIksanov@users.noreply.github.com>
2023-12-21 21:28:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function createSnowflake() {
 
   // Устанавливаем случайную скорость падения снежинки
   const speed = Math.random() * 5 + 1;
-  snowflake.style.animation = `snowfall ${speed}s linear infinite`;
+  snowflake.style.animation = `snowfall ${speed}s ease-in-out 0s 2 reverse`;
 
   // Добавляем снежинку на страницу
   document.body.appendChild(snowflake);
